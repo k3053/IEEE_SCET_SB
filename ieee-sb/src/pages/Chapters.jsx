42f5fb66a4b2abb069cb976fb4d8ac0a5ddc5b61
AyuSariya <aryansariya009@gmail.com>
2026-01-26 18:37:22 +0530

    Updated files: modified Chapters.jsx, updated package files, added VS Code launch config

--- ieee-sb/src/pages/Chapters.jsx
+++ ieee-sb/src/pages/Chapters.jsx
@@ -5,9 +5,12 @@ const Chapters = () => {
     <div className="container mx-auto mt-10">
       <h1 className="text-3xl font-bold">IEEE SCET SB Chapters</h1>
       <ul className="list-disc ml-10 mt-4">
-        <li>Computer Society</li>
-        <li>Electronics and Communication Society</li>
-        <li>Power and Energy Society</li>
+        <li>Computer Society (CS)</li>
+        <li>Women in Engineering (WIE)</li>
+        <li>Vehicular Technology Society (VTS)</li>
+        <li>Signal Processing Society (SPS)</li>
+        <li>Special Interest Group on Humanitarian Technology (SIGHT)</li>
+
         {/* Add more chapters */}
       </ul>
     </div>
